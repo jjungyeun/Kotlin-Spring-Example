89e26094fd8a6fc0108808415540881cf6b31b42
jjungyeun <wjyddd@naver.com>
2022-12-22 10:48:05 +0900

    [feat] 프로젝트 초기 생성

---
+++ src/test/kotlin/com/kongtech/example/ExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kongtech.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
